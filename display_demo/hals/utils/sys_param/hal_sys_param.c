6a79507751f03978970fe835a2fcbc48f3174703
SimonLi <likailong@huawei.com>
2021-12-13 14:31:16 +0800

    fix: 修正HalGetSerial接口，根据mac地址来获取唯一的serial number
    Signed-off-by: SimonLi <likailong@huawei.com>

--- display_demo/hals/utils/sys_param/hal_sys_param.c
+++ display_demo/hals/utils/sys_param/hal_sys_param.c
@@ -29,6 +29,18 @@
 #define OHOS_ABI_LIST "****"
 #define OHOS_SERIAL "1234567890"
 #define OHOS_FIRST_API_VERSION  1
+#define ETH_ALEN 6
+#define MAC_BITS 4
+#define HEX_A 0xa
+#define MAC_HIGH_MASK 0xf0
+#define MAC_LOW_MASK 0x0f
+#define CHAR_NUM_OFFSET 0x30
+#define CHAR_CAPITAL_OFFSET 0x37
+#define STR_END_FLAG '\0'
+
+typedef unsigned char               u8;
+
+static char serialNumber[2*ETH_ALEN + 1];
 
 const char* HalGetDeviceType(void)
 {
@@ -75,9 +87,35 @@ const char* HalGetHardwareProfile(void)
     return OHOS_HARDWARE_PROFILE;
 }
 
+static char Hex2Char(u8 hex)
+{
+    if (hex < HEX_A) {
+        return hex + CHAR_NUM_OFFSET;
+    } else {
+        return hex + CHAR_CAPITAL_OFFSET;
+    }
+}
+
 const char* HalGetSerial(void)
 {
-    return OHOS_SERIAL;
+    char macAddr[ETH_ALEN];
+    // as devboard has no production serial number, we just
+    // use wifi mac address as device serial number.
+    if (serialNumber[0] == STR_END_FLAG) {
+        extern int bwifi_get_own_mac(u8 *addr);
+        bwifi_get_own_mac(macAddr);
+        int j = 0;
+        for (int i = 0; i < ETH_ALEN; i++) {
+            u8 lowFour, highFour;
+            highFour = (macAddr[i] & MAC_HIGH_MASK) >> MAC_BITS;
+            serialNumber[j] = Hex2Char(highFour);
+            j++;
+            lowFour = macAddr[i] & MAC_LOW_MASK;
+            serialNumber[j] = Hex2Char(lowFour);
+            j++;
+        }
+    }
+    return serialNumber;
 }
 
 const char* HalGetBootloaderVersion(void)
