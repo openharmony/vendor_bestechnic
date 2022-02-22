a08ed2fc574a5221d454fc94c6d7168aa4be1f75
liubb_0516 <liubeibei8@huawei.com>
2022-02-22 11:50:47 +0800

    ipc MessageOption由枚举扩展为结构体
    Signed-off-by: liubb_0516 <liubeibei8@huawei.com>

--- display_demo/tests/rpc/src/rpc_client.c
+++ display_demo/tests/rpc/src/rpc_client.c
@@ -68,7 +68,9 @@ static void RpcClientMain(void)
 
     IpcIo reply2;
     uintptr_t ptr2 = 0;
-    MessageOption option = TF_OP_SYNC;
+    MessageOption option = {
+        .flags = TF_OP_SYNC
+    };
     int32_t ret = SendRequest(svc, OP_ADD, &data2, &reply2, option, &ptr2);
     if (ret != ERR_NONE) {
         RPC_LOG_INFO("OP_ADD failed");
